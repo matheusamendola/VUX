4b29dcbb7380af8dd2ff91f0b98d0e887396dbf6
Matheus Amendola <matheusamendolaa@gmail.com>
2025-12-08 23:23:15 -0300

    Reorganiza a função cmd_help e adiciona alias para o comando clear

--- kernel/commands.c
+++ kernel/commands.c
@@ -1,13 +1,5 @@
   #include "commands.h"
 
-void cmd_help(const char* arg) {
-    for (int i = 0; i < num_commands; i++) {
-        print(commands[i].name);
-        print(" - ");
-        print(commands[i].desc);
-        print("\n");
-    }
-}
 
 void cmd_echo(const char* arg) {
     print(arg);
@@ -18,6 +10,10 @@ void cmd_clear(const char* arg) {
     clear();
 }
 
+void cmd_cls(const char* arg) {
+    cmd_clear(arg);
+}
+
 void cmd_reboot(const char* arg) {
     outb(0x64, 0xFE);
 }
@@ -26,9 +22,19 @@ void cmd_shutdown(const char* arg) {
     outw(0x604, 0x2000);
 }
 
+void cmd_help(const char* arg) {
+    for (int i = 0; i < num_commands; i++) {
+        print(commands[i].name);
+        print(" - ");
+        print(commands[i].desc);
+        print("\n");
+    }
+}
+
 Command commands[] = {
     {"help",     "mostra esta ajuda",   cmd_help},
     {"clear",    "limpa a tela",        cmd_clear},
+    {"cls",      "alias para clear",    cmd_cls},
     {"reboot",   "reinicia o sistema",  cmd_reboot},
     {"shutdown", "desliga o sistema",   cmd_shutdown},
     {"echo",     "imprime texto",       cmd_echo},
